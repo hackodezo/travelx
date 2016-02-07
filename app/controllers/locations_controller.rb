64d864fcd4be0455e01c89cedf29bb3ee7fc27c9
Athul92 <athul.s@relyonsoft.com>
2016-02-07 10:28:09 +0530

    initial commit

---
+++ app/controllers/locations_controller.rb
@@ -0,0 +1,82 @@
+class LocationsController < ApplicationController
+  before_action :set_location, only: [:show, :edit, :update, :destroy]
+
+  # GET /locations
+  # GET /locations.json
+  def index
+    @locations = Location.all
+  end
+
+  # GET /locations/1
+  # GET /locations/1.json
+  def show
+     @location=Location.find(params[:id])
+     @results =JSON.parse(HTTParty.get("https://zilyo.p.mashape.com/search?latitude=12.9250074&longitude=77.5938028&maxdistance=20&resultsperpage=50&page=2",headers:{
+   "X-Mashape-Key" => "gtestym0nmmshZQTFzjLJ2kH7VTUp1y0daBjsnHTbCjBjNUf2Q",
+   "Content-Type" => "application/json"
+    }))
+
+     @r=@results["result"]
+     binding.pry
+  end
+
+  # GET /locations/new
+  def new
+    @location = Location.new
+  end
+
+  # GET /locations/1/edit
+  def edit
+  end
+
+  # POST /locations
+  # POST /locations.json
+  def create
+    @location = Location.new(location_params)
+
+    respond_to do |format|
+      if @location.save
+        format.html { redirect_to @location, notice: 'Location was successfully created.' }
+        format.json { render :show, status: :created, location: @location }
+      else
+        format.html { render :new }
+        format.json { render json: @location.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /locations/1
+  # PATCH/PUT /locations/1.json
+  def update
+    respond_to do |format|
+      if @location.update(location_params)
+        format.html { redirect_to @location, notice: 'Location was successfully updated.' }
+        format.json { render :show, status: :ok, location: @location }
+      else
+        format.html { render :edit }
+        format.json { render json: @location.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /locations/1
+  # DELETE /locations/1.json
+  def destroy
+    @location.destroy
+    respond_to do |format|
+      format.html { redirect_to locations_url, notice: 'Location was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_location
+      @location = Location.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def location_params
+      params.require(:location).permit(:address, :latitude, :longitude)
+    end
+end
